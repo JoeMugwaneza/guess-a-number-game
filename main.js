7110cccdf8a504bfc1095718ddfeee3b00202403
Mugwaneza Joseph <muganezajoseph@gmail.com>
2021-08-20 22:58:21 +0200

    guessing functionality developed; submitting a guess, checkng it and disabling when necessary

---
+++ main.js
@@ -0,0 +1,63 @@
+let randomNumber = Math.floor(Math.random()* 100) + 1;
+
+const guessField = document.querySelector('.guessField');
+const guessSubmit = document.querySelector('.guessSubmit');
+
+const guesses = document.querySelector('.guesses');
+const lastGuess = document.querySelector('.lastGuess');
+const lowOrHigh = document.querySelector('.lowOrHigh');
+
+let guessCount = 1; 
+let resetButton;
+
+// button was given ability to be click to activate guessing
+
+guessSubmit.addEventListener('click', checkGuess);
+
+// guessing fuction
+// 1. can given you all the records of guesses
+// 2. can tell you if you are wrong, below or above right guess
+// 3. can congratulate you when you win
+
+function checkGuess(){
+    let userGuess = Number(guessField.value);
+
+    if(guessCount === 1){
+        guesses.textContent = "Previous Guesses: ";
+    }
+
+    guesses.textContent += userGuess + " ";
+
+    if(userGuess < randomNumber){
+        lastGuess.textContent = "WRONG!"
+        lastGuess.style.backgroundColor = 'red';
+        lowOrHigh.textContent = "Last guess was too low!";
+    } else if(userGuess > randomNumber){
+        lastGuess.textContent = "WRONG!"
+        lastGuess.style.backgroundColor = 'red';
+        lowOrHigh.textContent = "Last guess was too high!";
+    } else{
+        // if userGuess is not less than or greater than random, means you won
+        gameWon();
+    }
+
+    function gameWon(){
+        guessField.disabled = true;
+        guessSubmit.disabled = true;
+        lastGuess.textContent = "Congratulations! You got it right!"
+        lastGuess.style.backgroundColor = 'green';
+        lowOrHigh.textContent = " ";
+    }
+
+    guessCount++;
+    resetGameOver();
+}
+
+function resetGameOver(){
+    if(guessCount === 10){
+        lastGuess.textContent = "!!!GAME OVER!!!";
+        guessField.disabled = true;
+        guessSubmit.disabled = true;
+        lowOrHigh.textContent = " ";
+    }
+}
