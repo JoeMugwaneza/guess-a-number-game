fb6c9840e8796fb99c981843cbb7726bfd7bc20a
Mugwaneza Joseph <muganezajoseph@gmail.com>
2021-08-22 10:41:01 +0200

    managed to refactor the code and created a new button that refresh the game so as to play again

--- main.js
+++ main.js
@@ -7,17 +7,16 @@ const guesses = document.querySelector('.guesses');
 const lastGuess = document.querySelector('.lastGuess');
 const lowOrHigh = document.querySelector('.lowOrHigh');
 
-let guessCount = 1; 
+let guessCount = 1;
 let resetButton;
 
-// button was given ability to be click to activate guessing
+// button is given ability to be clicked
 
-guessSubmit.addEventListener('click', checkGuess);
 
 // guessing fuction
 // 1. can given you all the records of guesses
 // 2. can tell you if you are wrong, below or above right guess
-// 3. can congratulate you when you win
+// 3. can congratulate you when you win.
 
 function checkGuess(){
     let userGuess = Number(guessField.value);
@@ -28,7 +27,6 @@ function checkGuess(){
     
     guesses.textContent += userGuess + " ";
    
-
     if(userGuess < randomNumber){
         lastGuess.textContent = "WRONG!"
         lastGuess.style.backgroundColor = 'red';
@@ -42,38 +40,52 @@ function checkGuess(){
         lowOrHigh.textContent = "Last guess was too high!";
         guessField.value = "";
         guessField.focus();
-    } else{
-        // if userGuess is not less than or greater than random, means you won
-        gameWon();
     }
 
-    function gameWon(){
-            guessField.disabled = true;
-            guessSubmit.disabled = true;
+    function gameOutcome(){
+        if(guessCount === 10){
+            lastGuess.textContent = "!!!GAME OVER!!!";
+            tryAgainButton();
+        } else if (userGuess === randomNumber){
             lastGuess.textContent = "Congratulations! You got it right!"
             lastGuess.style.backgroundColor = 'green';
-            lowOrHigh.textContent = " ";
-            tryAgain()
+            tryAgainButton();
+        }
     }
 
     guessCount++;
-    resetGameOver();
+    gameOutcome();
 }
+
+guessSubmit.addEventListener('click', checkGuess);
+
+// create button to refresh the game and start over
+function tryAgainButton(){
+    guessField.disabled = true;
+    guessSubmit.disabled = true;
+    lowOrHigh.textContent = " ";
+    resetButton = document.createElement('button');
+    resetButton.textContent = "Start New Game";
+    document.body.appendChild(resetButton);
+    resetButton.addEventListener('click', resetGameOver)
+}
+
+// reset recorded guesses to start fresh
 
 function resetGameOver(){
-    if(guessCount === 10){
-        lastGuess.textContent = "!!!GAME OVER!!!";
-        guessField.disabled = true;
-        guessSubmit.disabled = true;
-        lowOrHigh.textContent = " ";
-       
+    guessCount = 1;
+    let resetResultSections = document.querySelectorAll('resultSections p');
+    for (let i = 0; i < resetResultSections.length; i++){
+        resetResultSections[i].textContent = " ";
     }
-    tryAgain();
-}
 
-function tryAgain(){
-    resetButton.createElement('button');
-    resetButton.textContent = "Start Again";
-    document.body.append(resetButton)
-    resetButton.addEventListener('click', resetGame);
+    resetButton.parentNode.removeChild(resetButton);
+    guesses.textContent = "";
+    guessSubmit.disabled = false;
+    guessField.disabled = false;
+    lastGuess.textContent = "";
+    guessField.value = "";
+    guessField.focus();
+    lastGuess.style.backgroundColor = "white";
+    randomNumber = Math.floor(Math.random()*100)+ 1;
 }
